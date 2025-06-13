996dd6a26f0d564711c9108593cee2c2562fe9ac
yofrelopez <c.yofrelopez@gmail.com>
2025-06-13 18:14:42 -0500

    Desactiva ESLint en next.config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
   images: {
     domains: ['res.cloudinary.com'],
-  }
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
